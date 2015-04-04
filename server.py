e7dceff1f57433ea25258c31d2ec42725f7b023e
Justin Moen <jamoen7@gmail.com>
2015-04-03 21:34:45 -0700

    Progress creating, resizing and appending canvas element to DOM

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ define("port", default=8889, help="run on the given port", type=int)
 
 class UploadHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('upload.html')
+        self.render('templates/upload.html')
 
     def post(self):
         if 'file' in self.request.files.keys():
@@ -23,11 +23,22 @@ class UploadHandler(tornado.web.RequestHandler):
 
         self.write('uploading')
 
+class IcemanHandler(tornado.web.RequestHandler):
+    def get(self):
+        """serve the Iceman for now, other stuff later"""
+        f = Image.open('images/iceman.jpg')
+        o = io.BytesIO()
+        f.save(o, format="JPEG")
+        s = o.getvalue()
+        self.set_header('Content-type', 'image/jpg')
+        self.set_header('Content-length', len(s))
+        self.write(s)
 
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application([
         ("/", UploadHandler),
+        ('/iceman', IcemanHandler),
     ], debug=True)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
